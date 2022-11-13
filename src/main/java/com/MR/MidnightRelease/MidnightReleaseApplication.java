cf3d38960667acedf0ed21964610346bac19a971
Lucas Abeln <lucasabeln@gmail.com>
2022-11-13 10:24:49 -0800

    Base App

---
+++ src/main/java/com/MR/MidnightRelease/MidnightReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.MR.MidnightRelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidnightReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidnightReleaseApplication.class, args);
+	}
+
+}
