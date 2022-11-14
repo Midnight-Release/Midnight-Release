7fe53169aa02fdb31b0f339c8f879faca278835a
Lucas Abeln <lucasabeln@gmail.com>
2022-11-13 18:45:44 -0800

    Running properly

---
+++ src/main/java/com/MR/MidnightRelease/webservice/WebServiceController.java
@@ -0,0 +1,9 @@
+package com.MR.MidnightRelease.webservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WebServiceController {
+}
